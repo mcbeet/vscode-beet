915a4bb114cab506e364bd1cb51aa8b4ed2c0fb1
Oran9eUtan <Oran9eUtan@gmail.com>
2021-03-24 19:13:23 +0100

    feat: Beet is executed with interpreter at python.pythonPath

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,37 @@
 import * as vscode from 'vscode';
 
-let buildTask = beetTask("build");
-let cacheTask = beetTask("cache");
-let clearCacheTask = beetTask("cache", ["-c"]);
-let linkTask = beetTask("link");
-let clearLinkTask = beetTask("link", ["-c"]);
-let watchTask = beetTask("watch");
+let buildTask: vscode.Task;
+let cacheTask: vscode.Task;
+let clearCacheTask: vscode.Task;
+let linkTask: vscode.Task;
+let clearLinkTask: vscode.Task;
+let watchTask: vscode.Task;
 
 export function activate(ctx: vscode.ExtensionContext) {
+    generateBeetTasks();
+    registerBeetTasks(ctx);
+
+    vscode.workspace.onDidChangeConfiguration((e) => {
+        if (e.affectsConfiguration("python.pythonPath")) {
+            generateBeetTasks();
+        }
+    });
+}
+
+export function deactivate() {}
+
+function generateBeetTasks() {
+    let python = vscode.workspace.getConfiguration("python").get("pythonPath", "python");
+
+    buildTask = createBeetTask(python, "build");
+    cacheTask = createBeetTask(python, "cache");
+    clearCacheTask = createBeetTask(python, "cache", ["-c"]);
+    linkTask = createBeetTask(python, "link");
+    clearLinkTask = createBeetTask(python, "link", ["-c"]);
+    watchTask = createBeetTask(python, "watch");
+}
+
+function registerBeetTasks(ctx: vscode.ExtensionContext) {
     ctx.subscriptions.push(vscode.commands.registerCommand("vscode-beet.build", () => {
         vscode.tasks.executeTask(buildTask);
     }));
@@ -33,15 +57,13 @@ export function activate(ctx: vscode.ExtensionContext) {
     }));
 }
 
-export function deactivate() {}
-
-function beetTask(taskName: string, args: string[] = []) {
+function createBeetTask(python: string, taskName: string, args: string[] = []) {
     let task = new vscode.Task(
         {type: "beet", "task": taskName},
         vscode.TaskScope.Workspace,
         taskName,
         "beet",
-        new vscode.ProcessExecution("${config:python.pythonPath}", ["-m", "beet", taskName].concat(args)),
+        new vscode.ProcessExecution(python, ["-m", "beet", taskName].concat(args)),
     );
 
     task.presentationOptions.clear = true;
