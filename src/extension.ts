b9fdf59e6dad1b33823d6e4ee40a343258e04b12
Oran9eUtan <Oran9eUtan@gmail.com>
2021-03-24 14:53:46 +0100

    feat: Added beet tasks

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,46 @@
 import * as vscode from 'vscode';
 
+let buildTask = beetTask("build");
+let cacheTask = beetTask("cache");
+let clearCacheTask = beetTask("cache", ["-c"]);
+let linkTask = beetTask("link");
+let clearLinkTask = beetTask("link", ["-c"]);
+let watchTask = beetTask("watch");
+
 export function activate(ctx: vscode.ExtensionContext) {
+    ctx.subscriptions.push(vscode.commands.registerCommand("vscode-beet.build", () => {
+        vscode.tasks.executeTask(buildTask);
+    }));
+
+    ctx.subscriptions.push(vscode.commands.registerCommand("vscode-beet.cache", () => {
+        vscode.tasks.executeTask(cacheTask);
+    }));
+
+    ctx.subscriptions.push(vscode.commands.registerCommand("vscode-beet.clear-cache", () => {
+        vscode.tasks.executeTask(clearCacheTask);
+    }));
+
+    ctx.subscriptions.push(vscode.commands.registerCommand("vscode-beet.link", () => {
+        vscode.tasks.executeTask(linkTask);
+    }));
+
+    ctx.subscriptions.push(vscode.commands.registerCommand("vscode-beet.clear-link", () => {
+        vscode.tasks.executeTask(clearLinkTask);
+    }));
+
+    ctx.subscriptions.push(vscode.commands.registerCommand("vscode-beet.watch", () => {
+        vscode.tasks.executeTask(watchTask);
+    }));
 }
 
 export function deactivate() {}
+
+function beetTask(task: string, args: string[] = []) {
+    return new vscode.Task(
+        {type: "beet", "task": task},
+        vscode.TaskScope.Workspace,
+        task,
+        "beet",
+        new vscode.ProcessExecution("beet", [task].concat(args))
+    );
+}
