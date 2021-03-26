2e15b940dedb1e92c0e9dff0841f1fe0b6ad1e94
Oran9eUtan <Oran9eUtan@gmail.com>
2021-03-26 10:26:27 +0100

    Moved pickFile to utils.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 import * as beet from './beet';
 import * as config from './config';
+import * as utils from './utils';
 
 let cacheTask: vscode.Task;
 let clearCacheTask: vscode.Task;
@@ -44,25 +45,6 @@ function updateBeetTasks() {
     watchTask = beet.createTask(python, "watch", ["watch"]);
 }
 
-async function pickFile(files: vscode.Uri[], placeHolder: string): Promise<vscode.Uri | undefined> {
-    if (files.length === 0) {
-        return undefined;
-    } else if(files.length === 1) {
-        return files[0];
-    }
-
-    let options: { [key: string]: (vscode.Uri) } = {};
-    files.filter((f) => f.scheme === "file").forEach((f) => {
-        options[vscode.workspace.asRelativePath(f.fsPath)] = f;
-    });
-
-    return vscode.window.showQuickPick(Object.keys(options).map((label) => ({ label })), { placeHolder }).then((selection) => {
-        if (selection) {
-            return options[selection.label];
-        }
-    });
-}
-
 async function build() {
     let configFiles = await beet.getConfigFiles();
 
@@ -74,7 +56,7 @@ async function build() {
         case 1:
             configFile = configFiles[0];
         default:
-            let selection = await pickFile(configFiles, "Pick beet config file");
+            let selection = await utils.pickFile(configFiles, "Pick beet config file");
             if(!selection) {
                 return;
             }
--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,37 @@
+import * as vscode from 'vscode';
 import * as fs from 'fs';
+import * as util from 'util';
 
-export function isDirectory(path: string) {
+export async function isDirectory(path: string) {
     try {
-        const stats = fs.statSync(path);
-        return stats.isDirectory();
+        const stat = await util.promisify(fs.stat)(path);
+        return stat.isDirectory();
     } catch(e) {
         return false;
     }
 };
 
-export function listDir(path: string) {
+export async function listDir(path: string) {
     try {
-        return fs.readdirSync(path);
+        return await util.promisify(fs.readdir)(path);
     } catch(e) {
         return [];
     }
+}
+
+export async function pickFile(files: vscode.Uri[], placeHolder: string): Promise<vscode.Uri | undefined> {
+    if (files.length === 0) {
+        return undefined;
+    } else if(files.length === 1) {
+        return files[0];
+    }
+
+    let options: { [key: string]: (vscode.Uri) } = {};
+    files.filter((f) => f.scheme === "file").forEach((f) => {
+        options[vscode.workspace.asRelativePath(f.fsPath)] = f;
+    });
+
+    return vscode.window.showQuickPick(Object.keys(options).map((label) => ({ label })), { placeHolder }).then((selection) => {
+        return selection ? options[selection.label] : undefined;
+    });
 }
